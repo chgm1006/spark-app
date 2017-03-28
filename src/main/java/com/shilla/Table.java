d040ca7686bda92b51a87c7186dd7b196e553e88
Forrest G. Choi <chgm1006@users.noreply.github.com>
2017-03-28 16:59:12 +0900

    Create Table.java

---
+++ src/main/java/com/shilla/Table.java
@@ -0,0 +1,44 @@
+package shilla;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Table implements Serializable {
+
+    String tableName;
+    String hdfsPath;
+    String delimiter;
+    List<Column> columns;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getHdfsPath() {
+        return hdfsPath;
+    }
+
+    public void setHdfsPath(String hdfsPath) {
+        this.hdfsPath = hdfsPath;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<Column> columns) {
+        this.columns = columns;
+    }
+}
